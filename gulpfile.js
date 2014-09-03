a243d0b4bec6d2ebca92db82bee3fd8f540e94d5
shinya takahashi <s.takahashi313@gmail.com>
2014-09-03 22:34:12 +0900

    refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,11 @@ var source     = require('vinyl-source-stream');
 var glob       = require('glob');
 
 gulp.task('build', function(){
-  //var files = glob.sync('./frontend/**/*.js');
-  //return browserify(files).transform(partialify)
-  return browserify('./frontend/index.js').transform(partialify)
-                          .bundle()
-                          .pipe(source('browserified.js'))
-                          .pipe(gulp.dest('./app/assets/javascripts'));
+  browserify('./frontend/index.js')
+  .transform(partialify)
+  .bundle()
+  .pipe(source('browserified.js'))
+  .pipe(gulp.dest('./app/assets/javascripts'));
 });
 
 gulp.task('watch', ["build"], function(){
